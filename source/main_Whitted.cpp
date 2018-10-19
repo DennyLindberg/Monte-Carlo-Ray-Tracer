36a894aafd29c56be1589275700167b703b26acb
Denny Lindberg <DennyLindberg@users.noreply.github.com>
2018-10-19 18:48:33 +0200

    Added multi-threaded support for tracing. Each thread works on its own region in the image.

--- source/main_Whitted.cpp
+++ source/main_Whitted.cpp
@@ -15,35 +15,6 @@
 #include "core.h"
 
 #include <thread>
-struct ThreadInfo
-{
-	int threadId = 0;
-	int xBegin = 0;
-	int xEnd = 0;
-	int yBegin = 0;
-	int yEnd = 0;
-};
-/*
-	const int numThreads = std::thread::hardware_concurrency();
-	
-	ApplicationClock performanceTimer;
-	performanceTimer.Tick();
-
-	std::vector<std::thread> t(numThreads);
-	std::vector<double> maxValues(numThreads);
-	for (int i = 0; i < numThreads; i++)
-	{
-		ThreadInfo info = { &buffer, &glImage, &maxValues[i], i, numThreads };
-		t[i] = std::thread(DetermineMaxValue_threaded, info);
-	}
-	for (int i = 0; i < numThreads; i++)
-	{
-		t[i].join();
-	}
-
-	performanceTimer.Tick();
-	std::cout << "Image conversion (" + (enableMultithreading ? (std::to_string(numThreads) + " threads") : "single thread") + "): " + std::to_string(performanceTimer.DeltaTime()) + " s\r\n";
-*/
 
 static const bool SCREEN_VSYNC = false;
 static const unsigned int SCREEN_FULLSCREEN = 0;
@@ -56,11 +27,83 @@ static const unsigned int RAY_TRACE_DEPTH = 2;
 
 static const float CAMERA_FOV = 90.0f;
 
-static const float SCREEN_UPDATE_DELAY = 2.0f;
+static const float SCREEN_UPDATE_DELAY = 0.0f;
+
 static const bool USE_MULTITHREADING = true;
+typedef std::vector<std::thread> ThreadVector;
+const unsigned int NUM_SUPPORTED_THREADS = std::thread::hardware_concurrency();
+ThreadVector threads(NUM_SUPPORTED_THREADS);
+
+/*
+	Multi-threading details
+*/
+struct ThreadInfo
+{
+	unsigned int id = 0;
+	Camera* camera = nullptr;
+	Scene* scene = nullptr;
+	GLFullscreenImage* glImage = nullptr;
+	bool loop = false;
+};
+
+void StopThreads(ThreadVector& threads)
+{
+	for (std::thread& thread : threads)
+	{
+		thread.~thread();
+	}
+}
+
+/*
+	Main ray tracing function
+*/
+void TraceRegion(unsigned int yBegin, unsigned int yEnd, Camera& camera, Scene& scene, GLFullscreenImage& glImage)
+{
+	Ray cameraRay;
+	ColorDbl rayColor;
+
+	for (unsigned int y = yBegin; y < yEnd; ++y)
+	{
+		for (unsigned int x = 0; x < SCREEN_WIDTH; ++x)
+		{
+			cameraRay = camera.GetPixelRay(x + 0.5f, y + 0.5f);
+
+			if constexpr (RAY_TRACE_UNLIT)
+			{
+				rayColor = scene.TraceUnlit(cameraRay);
+			}
+			else
+			{
+				rayColor = scene.TraceRay(cameraRay, RAY_TRACE_DEPTH);
+			}
+
+			camera.pixels.SetPixel(x, y, rayColor);
+			glImage.buffer.SetPixel(x, y, rayColor.r, rayColor.g, rayColor.b, 1.0);
+		}
+	}
+}
+
+void TraceRegionThreaded(ThreadInfo thread)
+{
+	unsigned int yStep = SCREEN_HEIGHT / NUM_SUPPORTED_THREADS;
+
+	unsigned int yBegin = yStep* thread.id;
+	unsigned int yEnd = yBegin + yStep;
+
+	if (thread.id == NUM_SUPPORTED_THREADS - 1)
+	{
+		yEnd = SCREEN_HEIGHT;
+	}
+
+	do
+	{
+		TraceRegion(yBegin, yEnd, *thread.camera, *thread.scene, *thread.glImage);
+	} while (thread.loop);
+}
 
 int main()
 {
+
 	OpenGLWindow window("OpenGL", SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_FULLSCREEN, SCREEN_VSYNC);
 	window.SetClearColor(0.0, 0.0, 0.0, 1.0f);
 	window.Clear();
@@ -89,57 +132,59 @@ int main()
 	bool quit = false;
 	while (!quit)
 	{
-		clock.Tick();
-		window.SetTitle("FPS: " + std::to_string(1 / clock.DeltaTime()) + " - Time: " + std::to_string(clock.Time()));
-
-		/*
-			Ray tracing loop
-
-			Exit loop once we have reached SCREEN_UPDATE_DELAY.
-		*/
-		//while ((clock.Time() - lastScreenUpdate) >= SCREEN_UPDATE_DELAY) 
-		//{}
-
 		if (!alreadyRendered)
 		{
 			alreadyRendered = true;
+			lastScreenUpdate = clock.Time();
 
-			/*
-				Let's run path tracing once per pixel as a test
-			*/
-			Ray cameraRay;
-			ColorDbl rayColor;
-			for (unsigned int y = 0; y < SCREEN_HEIGHT; ++y)
+			if (!USE_MULTITHREADING)
 			{
-				for (unsigned int x = 0; x < SCREEN_WIDTH; ++x)
+				TraceRegion(0, SCREEN_HEIGHT, camera, scene, glImage);
+				clock.Tick();
+			}
+			else
+			{
+				// Additional threads are created from id=1 because id=0 is the main thread.
+				for (unsigned int i = 1; i < NUM_SUPPORTED_THREADS; i++)
 				{
-					cameraRay = camera.GetPixelRay(x + 0.5f, y + 0.5f);
-
-					if constexpr(RAY_TRACE_UNLIT)
-					{
-						rayColor = scene.TraceUnlit(cameraRay);
-					}
-					else
-					{
-						rayColor = scene.TraceRay(cameraRay, RAY_TRACE_DEPTH);
-					}
-
-					camera.pixels.SetPixel(x, y, rayColor);
-					glImage.buffer.SetPixel(x, y, rayColor.r, rayColor.g, rayColor.b, 1.0);
+					ThreadInfo info = { i, &camera, &scene, &glImage, false };
+					threads[i] = std::thread(TraceRegionThreaded, info);
 				}
 
-				// Only render frame every fifth row
-				if (y % 5 == 0 || y == SCREEN_HEIGHT-1)
+				/*
+					Main thread has two jobs:
+						1. Render the first region
+						2. Update the screen
+
+					It will update the screen after each rendered row.
+				*/
+				unsigned int yStep = SCREEN_HEIGHT / NUM_SUPPORTED_THREADS;
+				for (unsigned int y = 1; y < yStep; y++)
 				{
-					//window.Clear();
-					glImage.Draw();
-					window.SwapFramebuffer();
-					lastScreenUpdate = clock.Time();
+					TraceRegion(y, y + 1, camera, scene, glImage);
+
+					clock.Tick();
+					if ((clock.Time() - lastScreenUpdate) >= SCREEN_UPDATE_DELAY)
+					{
+						window.SetTitle("FPS: " + std::to_string(1 / clock.DeltaTime()) + " - Time: " + std::to_string(clock.Time()));
+						glImage.Draw();
+						window.SwapFramebuffer();
+						lastScreenUpdate = clock.Time();
+					}
+				}
+			
+				// When main thread is done, wait for other threads to catch up
+				for (unsigned int i = 1; i < NUM_SUPPORTED_THREADS; i++)
+				{
+					threads[i].join();
 				}
 			}
+
+			window.SetTitle("FPS: " + std::to_string(1 / clock.DeltaTime()) + " - Time: " + std::to_string(clock.Time()));
 		}
 
-
+		glImage.Draw();
+		window.SwapFramebuffer();
 
 		/*
 			Handle input events
@@ -166,5 +211,6 @@ int main()
 		}
 	}
 
+	//StopThreads();
 	return 0;
 }
